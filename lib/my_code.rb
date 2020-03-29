f563a313aa709430ac0dd4cc3cccbeb66c15a22d
Darrick Pang <dkypang@yahoo.com>
2020-03-29 22:59:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,10 @@
-def map_to_negativize(arr)
-  
+def map_to_negativize(arr, num)
+  x = 0 
+  newarr = []
+  while x < arr.length do 
+    tot = arr[x]*num
+    newarr.push(tot)
+  end 
+  return newarr
 end 
+
