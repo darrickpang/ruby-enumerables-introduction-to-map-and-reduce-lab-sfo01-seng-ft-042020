cf3d0c3d145db2782493ff1869cd810e394e9604
Darrick Pang <dkypang@yahoo.com>
2020-03-29 23:33:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,3 +17,13 @@ def map_to_no_change(arr, num)
   end 
   return newarr
 end 
+
+def map_to_double(arr, num)
+  x = 0 
+  newarr = []
+  while x < arr.length do 
+    tot = arr[x]*num
+    newarr.push(tot)
+  end 
+  return newarr
+end 
