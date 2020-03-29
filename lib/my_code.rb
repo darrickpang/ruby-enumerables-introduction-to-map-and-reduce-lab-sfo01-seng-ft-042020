e4f6e93554cbd1829fc615b7384565cb8c3ee7fb
Darrick Pang <dkypang@yahoo.com>
2020-03-29 23:50:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,6 +12,7 @@ def map_to_no_change(arr)
   x = 0 
   while x < arr.length do 
     arr[x] = arr[x]
+    x +=1
   end 
   return arr
 end 
@@ -20,15 +21,16 @@ def map_to_double(arr)
   x = 0 
   while x < arr.length do 
     arr[x]*= 2
+    x +=1
   end 
   return arr
 end 
 
 def map_to_square(arr)
   x = 0 
-
   while x < arr.length do 
     arr[x] *= arr[x]
+    x += 1
   end 
   return newarr
 end 
