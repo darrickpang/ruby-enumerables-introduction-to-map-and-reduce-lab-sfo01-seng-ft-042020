1bdeb337ca08fe1ab3bb0ce279da3dc388c02669
Darrick Pang <dkypang@yahoo.com>
2020-03-30 00:28:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,7 +38,7 @@ end
 def reduce_to_total(arr, start)
   x = 0 
   while x < arr.length do 
-    if(start !== nil)
+    if(start !== 0)
       start = start + arr[x]
     else 
       start = start + arr[x]
